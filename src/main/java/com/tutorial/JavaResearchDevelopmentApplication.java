3564d90cfdd87f34f4506d61bbf384b9340fb5ad
Budhi Octaviansyah <budioct@gmail.com>
2024-02-21 11:45:17 +0700

    start project

---
+++ src/main/java/com/tutorial/JavaResearchDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaResearchDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaResearchDevelopmentApplication.class, args);
+	}
+
+}
