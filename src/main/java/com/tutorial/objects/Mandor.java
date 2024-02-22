2000ee8f024999c1f3892135f5c44bbaea4770ed
Budhi Octaviansyah <budioct@gmail.com>
2024-02-22 20:35:04 +0700

    java: multiple static initializers

---
+++ src/main/java/com/tutorial/objects/Mandor.java
@@ -0,0 +1,22 @@
+package com.tutorial.objects;
+
+public class Mandor {
+
+    /**
+     * Multiple Static Initializers
+     */
+
+    static {
+        System.out.println("s1");
+    }
+
+    static {
+        System.out.println("s2");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("in main");
+    }
+
+}
