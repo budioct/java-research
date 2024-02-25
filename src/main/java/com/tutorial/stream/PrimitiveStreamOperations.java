2ce6860a9e2474fc48249d9809a70caf87a5b2da
Budhi Octaviansyah <budioct@gmail.com>
2024-02-25 12:25:27 +0700

    java: how to calculate stats on integer list / stream

--- src/main/java/com/tutorial/stream/PrimitiveStreamOperations.java
+++ src/main/java/com/tutorial/stream/PrimitiveStreamOperations.java
@@ -1,6 +1,9 @@
 package com.tutorial.stream;
 
+import java.util.IntSummaryStatistics;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class PrimitiveStreamOperations {
 
@@ -14,9 +17,26 @@ public class PrimitiveStreamOperations {
         }
     }
 
+    static void howToCalculateOnIntegerString(Integer first, Integer second) {
+
+        // How To Calculate Stats on Integer List / Stream
+
+        Stream<Integer> boxed = IntStream.range(first, second).boxed(); // Stream<Integer> boxed() // convert to Stream<T>
+        IntSummaryStatistics collect = boxed.collect(Collectors.summarizingInt(n -> n));
+
+        System.out.println("Count: " + collect.getCount());
+        System.out.println("Sum: " + collect.getSum());
+        System.out.println("Average: " + collect.getAverage());
+        System.out.println("Max: " + collect.getMax());
+        System.out.println("Min: " + collect.getMin());
+    }
+
     public static void main(String[] args) {
 
-        PrimitiveStreamOperations.intStreamtoArray(0, 5);
+//        PrimitiveStreamOperations.intStreamtoArray(0, 5);
+        PrimitiveStreamOperations.howToCalculateOnIntegerString(0, 10);
+
+
     }
 
 }
