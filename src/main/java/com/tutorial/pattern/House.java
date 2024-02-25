fb90a918e7f220db129e43ca7b6cdd372e5a9b3a
Budhi Octaviansyah <budioct@gmail.com>
2024-02-25 12:00:13 +0700

    java: decorator pattern with java records

---
+++ src/main/java/com/tutorial/pattern/House.java
@@ -0,0 +1,25 @@
+package com.tutorial.pattern;
+
+public class House {
+
+    // Decorator Pattern with Java Records
+    public record Decorate(Runnable r) implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("House.Decorate.run()");
+            r.run();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Decorate d = new Decorate(() -> System.out.println("House.main()"));
+        d.run(); // running job runnable
+
+        /**
+         * result:
+         * House.Decorate.run()
+         * House.main()
+         */
+    }
+
+}
